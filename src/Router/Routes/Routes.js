fd2cb0407251e1ecbd0f163266c25f4973957b7a
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-10-30 22:26:42 +0600

    Basic Layout done

--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -1,7 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
+import Main from "../../Layout/Main";
 
 export const router = createBrowserRouter([
   {
-    path: '/'
+    path: '/',
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/'
+      }
+    ]
   }
 ]);
