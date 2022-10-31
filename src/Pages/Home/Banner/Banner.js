2e572ccfbdf6ee092b22a73a8fef23f436558482
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-10-31 16:51:08 +0600

    added theme to html and  added border radius to img and before

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -5,7 +5,6 @@ import img3 from "../../../assets/images/banner/3.jpg";
 import img4 from "../../../assets/images/banner/4.jpg";
 import img5 from "../../../assets/images/banner/5.jpg";
 import img6 from "../../../assets/images/banner/6.jpg";
-import "./Banner.css";
 import BannerItem from "./BannerItem";
 
 const bannerData = [
@@ -49,7 +48,7 @@ const bannerData = [
 
 const Banner = () => {
   return (
-    <div className="carousel w-full">
+    <div className="carousel w-full py-10">
       {bannerData.map((slide) => (
         <BannerItem key={slide.id} slide={slide}></BannerItem>
       ))}
--- src/Pages/Home/Banner/BannerItem.js
+++ src/Pages/Home/Banner/BannerItem.js
@@ -1,11 +1,12 @@
 import React from "react";
+import './BannerItem.css';
 
 const BannerItem = ({ slide }) => {
   const { image, prev, id, next } = slide;
   return (
     <div id={`slide${id}`} className="carousel-item relative w-full">
       <div className="carousel-img">
-        <img src={image} className="w-full rounded-lg" alt="" />
+        <img src={image} className="w-full rounded-xl" alt="" />
       </div>
       <div className="absolute flex justify-end transform -translate-y-1/2 left-24 top-1/4">
         <h1 className="text-6xl font-bold text-white">
