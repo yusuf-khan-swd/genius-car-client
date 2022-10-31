a209b72fbb080ead5d35962db4c0e9087fa53acc
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-10-31 14:57:44 +0600

    added text to first banner

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
     <li className='font-semibold'><Link to='/'>Home</Link></li>
   </>;
   return (
-    <div className="navbar h-20 mb-12 bg-base-100">
+    <div className="navbar h-20 mb-12 pt-12 bg-base-100">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
