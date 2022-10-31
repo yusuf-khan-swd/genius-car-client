21be49f767821c39dee3e3a96bdbf729ab29d08c
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-10-31 16:38:45 +0600

    remove comment

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -51,22 +51,9 @@ const Banner = () => {
 
   return (
     <div className="carousel w-full">
-
       {
         bannerData.map(slide => <BannerItem key={slide.id} slide={slide}></BannerItem>)
       }
-
-      {/* <BannerItem image={img1}></BannerItem> */}
-
-      {/* <div id="slide2" className="carousel-item relative w-full">
-        <img src={img2} className="w-full" alt="" />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide1" className="btn btn-circle">❮</a>
-          <a href="#slide3" className="btn btn-circle">❯</a>
-        </div>
-      </div> */}
-
-
     </div>
   );
 };
