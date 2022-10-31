b7b0af9bf920ef7b0295bb56a46e1d0c5724c1d9
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-10-31 20:40:27 +0600

    create user with sign up

--- src/contexts/AuthProvider/AuthProvider.js
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -1,6 +1,6 @@
-import React, { createContext, useState } from 'react';
-import { getAuth } from 'firebase/auth';
-import app from '../../firebase/firebase.config';
+import React, { createContext, useEffect, useState } from "react";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged } from "firebase/auth";
+import app from "../../firebase/firebase.config";
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
@@ -9,12 +9,26 @@ const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  const authInfo = { user, loading };
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, currentUser => {
+      console.log('Inside auth state', currentUser);
+      setUser(currentUser);
+    });
+
+    return () => {
+      return unsubscribe();
+    }
+
+  }, []);
+
+  const authInfo = { user, loading, createUser };
 
   return (
-    <AuthContext.Provider value={authInfo}>
-      {children}
-    </AuthContext.Provider>
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
 };
 
