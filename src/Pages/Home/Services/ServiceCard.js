c68ea63cb43c93f804e164741fe4112435ee708f
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-02 15:43:22 +0600

    load a specific service in checkout route from server

--- src/Pages/Home/Services/ServiceCard.js
+++ src/Pages/Home/Services/ServiceCard.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ServiceCard = ({ service }) => {
-  const { img, title, price } = service;
+  const { _id, img, title, price } = service;
 
   return (
     <div className="card card-compact w-96 bg-base-100 shadow-xl p-6">
@@ -10,7 +11,9 @@ const ServiceCard = ({ service }) => {
         <h2 className="card-title">{title}</h2>
         <p className='text-orange-600 text-2xl font-semibold'>Price: ${price}</p>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
+          <Link to={`/checkout/${_id}`}>
+            <button className="btn btn-primary">Buy Now</button>
+          </Link>
         </div>
       </div>
     </div>
