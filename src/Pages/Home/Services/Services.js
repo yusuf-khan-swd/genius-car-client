f437539a5af78e8f7df6a473f1e056f7d99b1e62
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-11 18:42:14 +0600

    load services from localhost

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -5,7 +5,7 @@ const Services = () => {
   const [services, setServices] = useState([]);
 
   useEffect(() => {
-    fetch('https://genius-car-server-pi-pearl.vercel.app/services')
+    fetch('http://localhost:5000/services')
       .then(res => res.json())
       .then(data => setServices(data))
   }, []);
